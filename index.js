52458e039d0a07036b2ebde881e9a89420c2fa65
Matius Rimbe <matiusrimbe.ptik@gmail.com>
2023-02-21 11:23:18 +0800

    fix bug and add javascript

---
+++ index.js
@@ -0,0 +1,87 @@
+const form = prompt("Masukkan Nama Anda");
+const element = document.getElementById("name");
+element.innerText = form;
+
+function validate() {
+  document.getElementById("error_name").style.display = "none";
+  document.getElementById("error_birth").style.display = "none";
+  document.getElementById("error_jk").style.display = "none";
+  document.getElementById("error_message").style.display = "none";
+}
+
+function btn_submit() {
+  const today = new Date();
+  let name = document.forms["form"]["full_name"].value;
+  let tgl_lahir = document.forms["form"]["tgl_lahir"].value;
+  let jenis_kelamin = document.forms["form"]["jenis_kelamin"].value;
+  let pesan = document.forms["form"]["pesan"].value;
+
+  const time_today = document.getElementById("time_today");
+  const result_name = document.getElementById("result_name");
+  const result_tgl_lahir = document.getElementById("result_tgl_lahir");
+  const result_jk = document.getElementById("result_jk");
+  const result_message = document.getElementById("result_message");
+
+  let error_name;
+  let error_birth;
+  let error_jk;
+  let error_message;
+
+  if (name == undefined || name.trim() == "") {
+    document.getElementById("error_name").style.display = "block";
+
+    error_name = "Nama lengkap harus diisi.";
+  } else {
+    error_name = "";
+  }
+  if (tgl_lahir == undefined || tgl_lahir.trim() == "") {
+    document.getElementById("error_birth").style.display = "block";
+
+    error_birth = "Tanggal lahir harus diisi.";
+  } else {
+    error_birth = "";
+  }
+  if (jenis_kelamin == undefined || jenis_kelamin.trim() == "") {
+    document.getElementById("error_jk").style.display = "block";
+
+    error_jk = "Jenis kelamin harus diisi.";
+  } else {
+    error_jk = "";
+  }
+  if (pesan == undefined || pesan.trim() == "") {
+    document.getElementById("error_message").style.display = "block";
+    error_message = "Pesan harus diisi.";
+  } else {
+    error_message = "";
+  }
+
+  const errors = {
+    full_name: error_name,
+    tgl_lahir: error_birth,
+    jenis_kelamin: error_jk,
+    pesan: error_message,
+  };
+
+  const e_error_name = document.getElementById("error_name");
+  const e_error_birth = document.getElementById("error_birth");
+  const e_error_jk = document.getElementById("error_jk");
+  const e_error_message = document.getElementById("error_message");
+
+  if (
+    errors.full_name == "" &&
+    errors.tgl_lahir == "" &&
+    errors.jenis_kelamin == "" &&
+    errors.pesan == ""
+  ) {
+    time_today.innerText = today;
+    result_name.innerText = name;
+    result_tgl_lahir.innerText = tgl_lahir;
+    result_jk.innerText = jenis_kelamin;
+    result_message.innerText = pesan;
+  } else {
+    e_error_name.innerText = errors.full_name;
+    e_error_birth.innerText = errors.tgl_lahir;
+    e_error_jk.innerText = errors.jenis_kelamin;
+    e_error_message.innerText = errors.pesan;
+  }
+}
